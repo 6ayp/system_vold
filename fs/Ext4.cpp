750ad16b190c37662ebc37d1e091d64653cab1a7
noname8964 <32409462+noname8964@users.noreply.github.com>
2018-10-22 13:45:00 +0800

    vold: Change ext4/f2fs secontext to media_rw for stock rom.

--- fs/Ext4.cpp
+++ fs/Ext4.cpp
@@ -139,7 +139,7 @@ status_t Mount(const std::string& source, const std::string& target, bool ro,
         if (!data.empty()) {
             data += ",";
         }
-        data += "context=u:object_r:sdcard_posix:s0";
+        data += "context=u:object_r:media_rw_data_file:s0";
     }
     const char* c_source = source.c_str();
     const char* c_target = target.c_str();
